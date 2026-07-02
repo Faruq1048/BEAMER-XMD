d88294a82c0c22c77464a4d0c2bd2397a022459d
Faruq1048 <daudafaruq2009@gmail.com>
2026-07-02 22:41:18 +0100

    Update ownerNumber and sessionID in settings.js

--- settings.js
+++ settings.js
@@ -1,4 +1,4 @@
 module.exports = {
-    ownerNumber: [''], // Put your number here e.g. ['2547XXXXXXXX'] MUST
-    sessionID: ''      // Paste your session ID here MUST
+    ownerNumber: ['2349029014526'], // Put your number here e.g. ['2547XXXXXXXX'] MUST
+    sessionID: ''blinder~HaBkTbQA#eKDiomL54OFae5knDFo9jLVlUj0JU_3zIsB5jv95bck    // Paste your session ID here MUST
 };

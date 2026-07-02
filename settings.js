c3cc9a3fef9a92ecad26b59a197eeeaf0c4f1db5
Faruq1048 <daudafaruq2009@gmail.com>
2026-07-02 21:51:52 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,4 +1,4 @@
 module.exports = {
-    ownerNumber: [''], // Put your number here e.g. ['2547XXXXXXXX'] MUST
-    sessionID: ''      // Paste your session ID here MUST
+    ownerNumber: [''], // Put your number here e.g. ['2349029014526'] MUST
+    sessionID: ''blinder~HaBkTbQA#eKDiomL54OFae5knDFo9jLVlUj0JU_3zIsB5jv95bck // Paste your session ID here MUST
 };
